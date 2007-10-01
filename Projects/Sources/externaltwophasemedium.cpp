1407865dd1eed388289745531a61fd7acb8658fe
fcasella <fcasella@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2007-10-01 21:32:42 +0000

    Fixed bug in equality test on the main trunk
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/trunk@1910 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Projects/Sources/externaltwophasemedium.cpp
+++ Projects/Sources/externaltwophasemedium.cpp
@@ -952,9 +952,9 @@ void setStateDefault_(BaseTwoPhaseMedium *medium, int choice, double d, double h
 		medium->setState_dT(d,T,phase);
 	else if(choice == CHOICE_ph)
 		medium->setState_ph(p,h,phase);
-	else if(choice = CHOICE_ps)
+	else if(choice == CHOICE_ps)
 		medium->setState_ps(p,s,phase);
-	else if(choice = CHOICE_pT)
+	else if(choice == CHOICE_pT)
 		medium->setState_pT(p,T);
 	else
 		errorMessage("Wrong choice of inputs in setStateDefault_()\n");
