bb925ae578aa66483e58f6808c7b154ff89e7f41
CRichter <CRichter@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2008-03-10 06:46:48 +0000

    added Visual Studio 2008 project
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/trunk@1923 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Projects/Sources/include.h
+++ Projects/Sources/include.h
@@ -40,7 +40,7 @@
   Set this preprocessor variable to 1 to include the interface to the
   FluidProp solver developed and maintained by Francesco Casella.
 */
-#define FLUIDPROP 0
+#define FLUIDPROP 1
 
 // Selection of build type for this project
 //! Build project into a DLL
@@ -50,7 +50,7 @@
   mechanism as well as the export statement.
   \sa BUILD_LIB
 */
-#define BUILD_DLL 0
+#define BUILD_DLL 1
 //! Build project into a LIB
 /*!
   Set this preprocessor variable to 1 if the project is built into a 
@@ -58,7 +58,7 @@
   mechanism as well as the export statement.
   \sa BUILD_DLL
 */
-#define BUILD_LIB 1
+#define BUILD_LIB 0
 
 //! Maximum number of non-overlapping transient medium objects
 /*!
