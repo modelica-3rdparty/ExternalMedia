73205f41c732863fa0a6112b18dc0d0e45df2328
fcasella <fcasella@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2012-02-20 10:56:22 +0000

    Updated documentation
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/branches/noUniqueID@4882 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Projects/Sources/documentation.h
+++ Projects/Sources/documentation.h
@@ -3,23 +3,79 @@
 
   \section intro_sec Introduction
   
-  Welcome to the ExternalMediaPackage HowTo guide. This package is meant to
-  help you interfacing your favourite external fluid computation code(s)
-  to Modelica, using the Modelica.Media interface. Currently, the
-  ExternalMedia library only supports two-phase media, using Dymola 6 and
-  Microsoft Visual Studio. In the future, we might add support for
-  single-phase pure and mixture media, as well as for OpenModelica and
-  gcc. Please contact the authors if you're interested in such
-  developments.
+  The ExternalMedia project was started in 2006 by Francesco Casella and 
+  Christoph Richter, with the aim of providing a framework for interfacing
+  external codes computing fluid properties to Modelica.Media-compatible
+  component models. The two main requirements are: maximizing the efficiency
+  of the code and minimizing the amount of extra code required to use your
+  own external code within the framework.
+  
+  The first implementation featured a hidden cache in the C++ layer and used
+  integer unique IDs to reference that cache. This architecture worked well
+  if the models did not contain implicit algebraic equations involving medium
+  properties, but had serious issues when such equations were involved, which
+  is often the case when solving steady-state initialization problems.
+  
+  The library has been restructured in 2012 by Francesco Casella and Roberto
+  Bonifetto. The main idea has been to get rid of the hidden cache and of the
+  unique ID references and use the Modelica state records for caching. In this
+  way, all optimizations performed by Modelica tools are guaranteed to give
+  correct results, which was previously not the case. The current
+  version implements the Modelica.Media.Interfaces.PartialTwoPhaseMedium
+  interface, so it can handle pure fluids, either one-phase or two-phase,
+  and is compatible with Modelica and Modelica Standard Library 3.2. Please note
+  that the paths of the medium packages have been changed from the previous
+  versions, so you might need some small changes if you want to upgrade your
+  models from previous versions of the ExternalMedia library.
+  
+  There are two ways to use this library. The easiest way is to use the
+  releases available on the Modelica website, which include a pre-compiled
+  interface to the FluidProp tool (http://www.fluidprop.com). FluidProp features
+  many built-in fluid models, and can optionally be used to access the whole
+  RefProp database, thus giving easy access to a wide range of fluid models
+  with state-of-the-art accuracy. If you want to use your own fluid property
+  computation code instead, then you need to check out the source code and
+  add the interface to it, as described in this manual.
+  
+  The library is released under Modelica License 2.0. 
+  
+  Please contact the main developer, Francesco Casella (casella@elet.polimi.it)
+  if you have questions or suggestions for improvement.
+  
+  Copyright (c) 2006-2012, Politecnico di Milano, TU Braunschweig, Politecnico
+  di Torino.
 
-  The ExternalMedia package has been implemented and is maintained by 
-  Francesco Casella (casella@elet.polimi.it) and Christoph Richter 
-  (ch.richter@tu-bs.de). The code is released under the Modelica license.
-
-  Copyright (c) 2006, Politecnico di Milano and TU Braunschweig.
-
+  \section releases_sec Using the pre-packaged releases with FluidProp
+  
+  Download and install the latest version of FluidProp from 
+  http://www.fluidprop.com. If you want to use the RefProp fluid models,
+  you need to get the full version of FluidProp, which has an extra license fee.
+  
+  Download and unzip the library corresponding to the version of Visual Studio
+  that you use to compile your Modelica models, in order to avoid linker errors.
+  Make sure that you load the ExternalMedia library in your Modelica tool
+  workspace, e.g. by opening the main package.mo file.
+  
+  You can now define medium models for all the libraries supported by FluidProp,
+  by extending the ExternalMedia.Media.FluidPropMedium package. Set libraryName
+  to "FluidProp.RefProp", "FluidProp.StanMix", "FluidProp.TPSI", "FluidProp.IF97",
+  or "FluidProp.GasMix" depending on the specific library you need to use.
+  Set substanceNames to a single-element string array containing the name
+  of the specific medium, as specified by the FluidProp documentation. Set
+  mediumName to a string that describes the medium (this only used for
+  documentation purposes but has no effect in selecting the medium model). 
+  See ExternalMedia.Examples for examples.
+  
+  Please note that the medium models IF97 and GasMix are already available
+  natively in Modelica.Media as Water.StandardWater and IdealGases.MixtureGases,
+  respectively - it is recommended to use the Modelica.Media models, since they
+  are much faster to compute.
+  
   \section architecture_sec Architecture of the package
 
+  This section gives an overview of the package structure, in order to help
+  you understand how to interface your own code to Modelica using it.
+  
   At the top level there is a Modelica package (ExternalMedia), which
   contains all the basic infrastructure needed to use external fluid
   properties computation software through a Modelica.Media compliant
@@ -33,130 +89,100 @@
   handled by the underlying C code to select the appropriate external
   code to use.
 
-  The functions within ExternalTwoPhaseMedium talk to a C/C++ interface
-  layer (called externalmedialib.cpp) via external functions
-  calls. This layer takes care of initializing the external packages, and
-  maintains a collection of C++ objects (stored within a MediumMap C++
-  object), corresponding to different instances of the medium model. More
-  specifically, each instance of ExternalTwoPhaseMedium.BaseProperties
-  corresponds to an instance of the C++ object TwoPhaseMedium, identified
-  by an integer uniqueID; each TwoPhaseMedium object also contains a
-  pointer to a C++ object inheriting from BaseSolver; this is a wrapper
-  object around the actual external code (or solver) which has to be
-  called to compute the fluid properties. It is possible to use multiple
-  instances of possibly different solvers at the same time; the
-  collection of active solver is managed by the SolverMap C++ object.
+  The Modelica functions within ExternalTwoPhaseMedium communicate to a
+  C/C++ interface layer (called externalmedialib.cpp) via external C functions
+  calls, which in turn make use of C++ objects. This layer takes care of
+  initializing the external fluid computation codes, called solvers from now on.
+  Every solver is wrapped by a C++ class, inheriting from the BaseSolver C++
+  class. The C/C++ layer maintains a set of active solvers, one for each
+  different combination of the libraryName and mediumName strings, by means of
+  the SolverMap C++ class. The key to each solver in the map is given by those
+  strings. It is then possible to use multiple instances of many solvers in the
+  same Modelica model at the same time.
+  
+  All the external C functions pass the libraryName, mediumName and substanceNames
+  strings to the corresponding functions of the interface layer. These in turn
+  use the SolverMap object to look for an active solver in the solver map,
+  corresponding to those strings. If one is found, the corresponding function
+  of the solver is called, otherwise a new solver object is instantiated and
+  added to the map, before calling the corresponding function of the solver.
+  
+  The default implementation of an external medium model is implemented by the
+  ExternalTwoPhaseMedium Modelica package. The setState_xx() and setSat_x()
+  function calls are rerouted to the corresponding functions of the solver
+  object. These compute all the required properties and return them in the
+  ExternalThermodynamicState and  ExternalSaturationProperties C structs, which
+  map onto the corresponding ThermodynamicState and SaturationProperties
+  records defined in ExternalTwoPhaseMedium. All the functions returning
+  properties as a function of the state records are implemented in Modelica and
+  simply return the corresponding element in the state record, which acts as
+  a cache. This is an efficient implementation for many complex fluid models, 
+  where most of the CPU time is spent solving the basic equation of state, while
+  the computation of all derived properties adds a minor overhead, so it makes
+  sense to compute them once and for all when the setState_XX() or setSat_xx()
+  functions are called. 
 
-  The default implementation of the external medium works as follows. At
-  initialization, each instance of the BaseProperties model calls the
-  createMedium_() function of the interface layer. This triggers the
-  allocation within the MediumMap object of a new TwoPhaseMedium object,
-  which will contain all the medium properties, and a pointer to the
-  appropriate solver. This object will be stored in the MediumMap, and
-  identified with a uniqueID, which is returned by the createMedium()
-  function and stored within the BaseProperties model. The&nbsp; solver
-  object is created by the SolverMap object, based on the mediumName,
-  libraryName and substanceName constants of the Modelica medium package
-  - only one solver object is created for multiple instances of the same
-  external medium.
+  In case some of the thermodynamic properties require a significant amount of
+  CPU time on their own, it is possible to override this default implementation.
+  On one hand, it is necessary to extend the ExternalTwoPhaseMedium Modelica
+  package and redeclare those functions, so that they call the corresponding
+  external C functions defined in externalmedium.cpp, instead of returning the
+  value cached in the state record. On the other hand, it
+  is also necessary to provide an implementation of the corresponding functions
+  in the C++ solver object, by overriding the virtual functions of the
+  BaseSolver object. In this case, the setState_xx() and setSat_X() functions
+  need not compute all the values of the cache state records; uncomputed
+  properties might be set to zero. This is not a problem, since Modelica.Media
+  compatible models should never access the elements of the state records
+  directly, but only through the appropriate functions, so these values should
+  never be actually used by component models using the medium package.
 
-  When one of the setState functions within BaseProperties is called, the
-  interface layer will call the corresponding setState function of the
-  TwoPhaseMedium object, which will in turn invoke the setState function
-  of the appropriat solver object, to actually compute the fluid
-  properties. These will be stored within the TwoPhaseMedium object, for
-  later retrieval. The setState function then returns a
-  ThermodynamicState record, which only contains integer uniqueID,
-  indentifying the TwoPhaseMedium object in the MediumMap. Subsequent
-  calls to compute any other functions of that state will just fetch the
-  corresponding values from the TwoPhaseMedium object with the given
-  uniqueID - the TwoPhaseMedium object acts like a cache for already
-  computed properties. The default implementation assumes that all
-  available properties are computed at once by the setState functions, so
-  that subsequent calls to any property function will just result in a
-  retrieval from the cache - more sophisticated implementations could be
-  devised, where the setState function only computes some properties at
-  first, and flags are provided within the TwoPhaseMedium object to keep
-  into account what has already been computed and stored in the cache,
-  and what has still to be computed if need.
-
-  With the current implementation, it is not possible to use setState
-  functions outside the BaseProperties model, unless one explicitly
-  manages and sets the uniqueID integers - this is not possible if the
-  user model has to be compatible with other Modelica.Media fluid models,
-  which do not have the uniqueID. Support for calls to setState functions
-  with automatic management of the uniqueID might be added in the future.
-
-  \section using_sec Using an already implemented external medium with Dymola
-
-  If somebody else has already implemented the external medium, you only need a few 
-  things to use it on your computer:
-
-  - the ExternalMedia Modelica package
-  - the ExternalTwoPhaseMedium.lib (and possibly the ExternalTwoPhaseMedium.dll) files, containing the compiled C/C++ code
-  - the externalmedialib.h C header file
-
-  Copy the ExternalTwoPhaseMedium.lib file within the Dymola/bin/lib
-  directory, the ExternalTwoPhaseMedium.dll file in the Windows/System32
-  directory (only if the library has been compiled to a DLL), and the
-  externalmedialib.h file in the Dymola/Source directory.
-
-  You can check that the compiler setup is working by running the
-  TestMedium model in the ExternalMedia.Test package - this will simulate
-  a dummy external fluid model, roughly corresponding to liquid water at
-  low pressure and temperature.
-
-  You can now use the external media provided in the ExternalMedia.Media
-  package. There are two options here: the first is to use
-  ExternalTwoPhaseMedium directly, using modifiers to change libraryName
-  and substanceNames to suit your needs; the other is to use or modify
-  one of the pre-packaged media. The libraryName string will be decoded
-  by the SolverMap object - for further details on the syntax of the
-  string, ask to the implementor of the interface to the code you'll
-  actually use.
-
-  \section development_sec Developing a new external medium model
+  \section development_sec Developing your own external medium package
 
   The ExternalMedia package has been designed to ease your task, so that
   you will only have to write the mimum amount of code which is strictly
   specific to your external code - everything else is already provided.
   The following instructions apply if you want to develop an external
   medium model which include a (sub)set of the functions defined in
-  Modelica.Media.Interfaces.PartialTwoPhaseMedium, and you want to
-  compute all the properties at once when you call the setState function
-  (this is a reasonable choice, since the majority of the time is
-  usually spent to run preliminary computations (e.g. compute the
-  Helmholtz function), and then computing all the properties only adds a
-  small overhead on top of that time).
-
-  First of all, you have to write you own solver object code; you can
+  Modelica.Media.Interfaces.PartialTwoPhaseMedium.
+  
+  The most straightforward implementation is the one in which all fluid
+  properties are computed at once by the setState_XX() and setSat_X() functions
+  and all the other functions return the values cached in the state records.
+  
+  First of all, you have to write you own solver object code: you can
   look at the code of the TestMedium and FluidPropMedium code as
   examples. Inherit from the BaseSolver object, which provides default
   implementations for most of the required functions, and then just add
   your own implementation for the following functions: object
   constructor, object destructor, setMediumConstants(), setSat_p(),
   setSat_T(), setState_ph(), setState_pT(), setState_ps(), setState_dT().
-  Note that the setState functions need to compute and fill in all
-  the fields of the& properties record, otherwise the corresponding
-  functions will not work. For example, if your setState functions do not
-  set the value of properties, then you won't be able to later
-  call thermalConductivity(medium.state) in your Modelica models. On the
-  other hand, you don't necessarily need to implement all of four
+  Note that the setState and setSat functions need to compute and fill in all
+  the fields of the corresponding C structs for the library to work correctly.
+  On the other hand, you don't necessarily need to implement all of the four
   setState functions: if you know in advance that your models will only
-  use&nbsp;certain combinations of variable as inputs (e.g. p, h), then
-  you might omit implementing the setState functions corresponding to the
-  other ones.
+  use certain combinations of variables as inputs (e.g. p, h), then
+  you might omit implementing the setState and setSat functions corresponding
+  to the other ones.
 
   Then you must modify the SolverMap::addSolver() function, so that it
   will instantiate your new solver when it is called with the appropriate
   libraryName string. You are free to invent you own syntax for the
   libraryName string, in case you'd like to be able to set up the
   external medium with some additional configuration data from within
-  Modelica - it's up to you to decode that syntax within the addSolver()
-  function, and within the constructor of your solver object.
+  Modelica - it is up to you to decode that syntax within the addSolver()
+  function, and within the constructor of your solver object. Look at how the
+  FluidProp solver is implemented for an example.
 
-  Finally, add the .cpp and .h files to the C/C++ project, recompile to a
-  static library (or to a DLL), and then follow the instructions of
-  Section 2. of this document.
-
+  Finally, add the .cpp and .h files of the solver object to the C/C++ project,
+  set the include.h file according to your needs and recompile it to a
+  static library (or to a DLL). The compiled libraries and the externalmedialib.h
+  files must then be copied into the Include subdirectory of the Modelica package
+  so that the Modelica tool can link them when compiling the models.
+  
+  As already mentioned in the previous section, you might provide customized
+  implementations where some of the properties are not computed by the setState
+  and setSat functions and stored in the cache records, but rather computed
+  on demand, based on a smaller set of thermodynamic properties computed by the
+  setState and setSat functions and stored in the state C struct.
 */
