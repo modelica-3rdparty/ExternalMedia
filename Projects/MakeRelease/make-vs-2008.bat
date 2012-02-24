b6458e8e28abf35a663121d1ba815581814e6b5a
fcasella <fcasella@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2012-02-24 17:33:32 +0000

    Changed directory name for release
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/branches/noUniqueID@4914 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Projects/MakeRelease/make-vs-2008.bat
+++ Projects/MakeRelease/make-vs-2008.bat
@@ -1,8 +1,8 @@
 REM builds release of ExternalMedia with lib file compiled by Visual Studio 2008
 
-rmdir /S /Q ExternalMediaVS2008
-mkdir ExternalMediaVS2008
-cd ExternalMediaVS2008
+rmdir /S /Q "ExternalMedia 3.2 VS2008"
+mkdir "ExternalMedia 3.2 VS2008"
+cd "ExternalMedia 3.2 VS2008"
 mkdir ExternalMedia
 cd ExternalMedia
 xcopy "..\..\..\..\Modelica\ExternalMedia 3.2\*.*" . /S
