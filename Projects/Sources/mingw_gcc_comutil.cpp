ea4e029c54e7083f7e348d9ff5504722fe0103ea
adrpo <adrpo@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2013-10-08 19:12:22 +0000

    - add Projects/Sources/mingw_gcc_comutil.cpp for undefined symbols - rename ExternalMediaLib.a to libExternalMediaLib.a - small changes to:   Projects/Sources/Makefile   Projects/make-gcc
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/trunk@7068 7ce873d0-865f-4ce7-a662-4bb36ea78beb

---
+++ Projects/Sources/mingw_gcc_comutil.cpp
@@ -0,0 +1,34 @@
+#if defined(__MINGW32__)
+
+#include <windows.h>
+#include <stdio.h>
+
+/* Support functions for _bstr_t in MinGW: Originally written by: Diaa Sami */
+void __stdcall _com_issue_error(HRESULT hr)
+{
+ printf("_com_issue_error() called with parameter HRESULT = %lu", hr);
+}
+
+namespace _com_util
+{
+ char * __stdcall ConvertBSTRToString(BSTR bstr)
+ {
+ const unsigned int stringLength = lstrlenW(bstr);
+ char *const ascii = new char [stringLength + 1];
+
+ wcstombs(ascii, bstr, stringLength + 1);
+
+ return ascii;
+ }
+
+ BSTR __stdcall ConvertStringToBSTR(const char *const ascii)
+ {
+ const unsigned int stringLength = lstrlenA(ascii);
+ BSTR bstr = SysAllocStringLen(NULL, stringLength);
+
+ mbstowcs(bstr, ascii, stringLength + 1);
+
+ return bstr;
+ }
+}
+#endif
