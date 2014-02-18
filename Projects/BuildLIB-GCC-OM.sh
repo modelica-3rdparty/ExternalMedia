61d0addb34f20a19c7e8e35d0bdc7b77d6087bda
belli <belli@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2014-02-18 15:04:20 +0000

    Added build script for ExternalMedia+CoolProp for OpenModelica
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/trunk@7297 7ce873d0-865f-4ce7-a662-4bb36ea78beb

---
+++ Projects/BuildLIB-GCC-OM.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+GCC_OPTS="-O2 -loleaut32 -DCOOLPROP=1"
+CP=../externals/coolprop/trunk
+CPinc=${CP}/CoolProp
+INCLUDES="-I${CPinc} -I${CP}/wrappers/Modelica/src"
+
+echo "Compiling sources"
+
+#  ****** compile all the sources from CoolProp for OpenModelica ************
+gcc ${GCC_OPTS} -c ${INCLUDES} ${CP}/wrappers/Modelica/src/coolpropsolver.cpp
+gcc ${GCC_OPTS} -c ${INCLUDES} Sources/*.cpp
+
+#  ********** CoolProp sources *********
+gcc ${GCC_OPTS} -c ${INCLUDES} ${CP}/CoolProp/*.cpp 
+
+ar -r libExternalMediaLib.a *.o
+ranlib libExternalMediaLib.a
+
+echo "Copying files to External Media 3.2 and 3.2.1"
+cp libExternalMediaLib.a "../Modelica/ExternalMedia 3.2/Resources/Library"
+cp Sources/externalmedialib.h "../Modelica/ExternalMedia 3.2/Resources/Include"
+cp libExternalMediaLib.a "../Modelica/ExternalMedia 3.2.1/Resources/Library"
+cp Sources/externalmedialib.h "../Modelica/ExternalMedia 3.2.1/Resources/Include"
+echo "All done"
