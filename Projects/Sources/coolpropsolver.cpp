3185d9922e290e30f707d4fae3b1f5b5db5008ac
jorrit <jorrit@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2014-11-10 17:50:44 +0000

    Added a larger delta p to the near crtical state in CoolProp. This should help with some initialisation problem.
    git-svn-id: https://svn.modelica.org/projects/ExternalMediaLibrary/trunk@7895 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Projects/Sources/coolpropsolver.cpp
+++ Projects/Sources/coolpropsolver.cpp
@@ -30,7 +30,7 @@ CoolPropSolver::CoolPropSolver(const std::string &mediumName, const std::string 
 	rho_smoothing_xend = 0;
 
 	// Initialise the saturation and near-critical variables
-	_p_eps   = 1e-5; // relative tolerance margin for subcritical pressure conditions
+	_p_eps   = 1e-3; // relative tolerance margin for subcritical pressure conditions
 	_delta_h = 1e-1; // delta_h for one-phase/two-phase discrimination
 
 	if (name_options.size()>1)
