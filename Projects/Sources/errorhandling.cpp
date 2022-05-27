0fbed6ce733a7fe26b2c2e02f6bf64cdbba9a4a9
Federico Terraneo <fede.tft@miosix.org>
2022-05-27 19:24:21 +0200

    Removed old error handling as suggested by Casella

--- Projects/Sources/errorhandling.cpp
+++ Projects/Sources/errorhandling.cpp
@@ -13,13 +13,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef EXTERNALMEDIA_MODELICA_ERRORS
-#define EXTERNALMEDIA_MODELICA_ERRORS 1
-#endif
-
-#if (EXTERNALMEDIA_MODELICA_ERRORS == 1)
+// Provides the IMPORT macro to make Modelica utility functions
+// available with a uniform interface for Windows and Linux
 #include "importer.h"
-// This implementation uses the native Modelica tool log and error window to report errors
+
 void errorMessage(char *errorMessage){
 	IMPORT(void (*)(const char *), ModelicaError);
 	ModelicaError(errorMessage);
@@ -29,16 +26,3 @@ void warningMessage(char *warningMessage){
 	IMPORT(void (*)(const char *), ModelicaMessage);
 	ModelicaMessage(warningMessage);
 }
-#else
-// This is the default section
-// Error and warnings are sent to the standard output
-void errorMessage(char *errorMessage){
-	printf("\a%s\nAn error occurred and the calculation cannot continue, please terminate your simulation manually.\n", errorMessage);
-	getchar();
-	exit(1);
-}
-
-void warningMessage(char *warningMessage){
-	printf("%s\n",warningMessage);
-}
-#endif
